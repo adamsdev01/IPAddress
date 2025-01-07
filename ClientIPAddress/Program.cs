97e3e46222d875524fa749633cdfb9663af82e2b
TA <tadams03@mail.roosevelt.edu>
2025-01-06 20:18:13 -0600

    fix code

--- ClientIPAddress/Program.cs
+++ ClientIPAddress/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 var app = builder.Build();
 
